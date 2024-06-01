5f251433324530646b7904ad78ad8dea96dbc976
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-06-01 04:20:05 +0300

    add code

--- light_pars/light_pars/spiders/l_pars.py
+++ light_pars/light_pars/spiders/l_pars.py
@@ -1,27 +1,60 @@
 import scrapy
+from openpyxl import Workbook
 
 
 class LParsSpider(scrapy.Spider):
     name = "l_pars"
-    allowed_domains = ["https://www.divan.ru/"]
+    allowed_domains = ["divan.ru"]
     start_urls = ["https://www.divan.ru/category/svet/page-1"]
+    custom_settings = {
+        'FEEDS': {
+            'output.json': {
+                'format': 'json',
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+            },
+        }
+    }
+
+    def __init__(self):
+        self.workbook = Workbook()
+        self.sheet = self.workbook.active
+        self.sheet.append(["Name", "Price", "URL"])
 
     def parse(self, response):
-        lights = []
-        try:
-            for i in range(100):
-                url = f"https://www.divan.ru/category/svet/page-{i + 1}"
-                light = response.css('div.lsooF')
-                lights.append(light)
-        except IndexError:
-            return 0
-        a = []
-        for li in lights:
-            for l in li:
-                a.append(l)
-                yield {
-                    "name": l.css('div.lsooF span::text').get(),
-                    "price": l.css('div.q5Uds span::text').get(),
-                    "url": l.css('a').attrib['href']
-                }
-        print(len(a))
+        lights = response.css('div.lsooF')
+
+        for light in lights:
+            name = light.css('span::text').get()
+            price = light.css('div.q5Uds span::text').get()
+            url = response.urljoin(light.css('a').attrib['href'])
+
+            # Сохраняем данные в XLSX
+            self.sheet.append([name, price, url])
+
+            # Возвращаем данные для экспорта в JSON
+            yield {
+                "name": name,
+                "price": price,
+                "url": url
+            }
+
+        # Переход на следующую страницу
+        response.follow('https://www.divan.ru/category/svet/page-2')
+        for i in range(7):
+            pages = response.css('div.ui-jDl24')
+
+            url = pages.css('a').attrib['href']
+            print(url)
+            url = response.urljoin(pages.css('a').attrib['href'])
+            response.follow(url)
+        next_page = response.css('a.next::attr(href)').get()
+        #if next_page is not None:
+         #   print(next_page)
+          #  yield response.follow(next_page, self.parse)
+
+    def closed(self, reason):
+        self.workbook.save("output.xlsx")
+
